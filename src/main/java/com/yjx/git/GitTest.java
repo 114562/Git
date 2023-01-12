1142a69d6dcd1b2d64fe2352707945f1a463ee55
Teddy <2372256530@qq.com>
2023-01-12 15:06:24 +0800

    second commit

--- src/main/java/com/yjx/git/GitTest.java
+++ src/main/java/com/yjx/git/GitTest.java
@@ -3,5 +3,6 @@ package com.yjx.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git,this is my first git written by IDEA");
+        System.out.println("hello git2");
     }
 }
