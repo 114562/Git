220368eb3c6eb600b77d2cf72a0a529370467b89
Teddy <2372256530@qq.com>
2023-01-12 16:00:36 +0800

    hot-fix commit1

--- src/main/java/com/yjx/git/GitTest.java
+++ src/main/java/com/yjx/git/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git,this is my first git written by IDEA");
         System.out.println("hello git2");
-        System.out.println("hello git3");
+        System.out.println("hello git4 hot fix");
     }
 }
