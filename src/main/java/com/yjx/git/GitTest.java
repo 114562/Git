0a1b03c19244cfc037aac0fa9ea0e6c4539c212a
Teddy <2372256530@qq.com>
2023-01-12 15:07:28 +0800

    third commit

--- src/main/java/com/yjx/git/GitTest.java
+++ src/main/java/com/yjx/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git,this is my first git written by IDEA");
         System.out.println("hello git2");
+        System.out.println("hello git3");
     }
 }
